df90c8da0e22e0922f764f206cdf67b2b168af44
abutieymahappen <happymahappen101@gmail.com>
2026-06-10 04:42:14 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,71 +1,86 @@
 import express from "express"
-import cors from "cors"
 import makeWASocket, {
-  useMultiFileAuthState,
-  fetchLatestBaileysVersion
+useMultiFileAuthState,
+fetchLatestBaileysVersion
 } from "@whiskeysockets/baileys"
 import Pino from "pino"
 
 const app = express()
-
-app.use(cors())
-
-const PORT = process.env.PORT || 8080
+const PORT = process.env.PORT || 10000
 
 app.get("/", (req, res) => {
-  res.send("AKATSUKII-MD ONLINE ✅")
-})
-
-async function startBot(number) {
-
-  const { state, saveCreds } =
-    await useMultiFileAuthState("./session")
-
-  const { version } =
-    await fetchLatestBaileysVersion()
-
-  const sock = makeWASocket({
-    version,
-    logger: Pino({ level: "silent" }),
-    auth: state,
-    browser: ["AKATSUKII-MD", "Chrome", "1.0.0"]
-  })
-
-  sock.ev.on("creds.update", saveCreds)
-
-  await new Promise(resolve =>
-    setTimeout(resolve, 5000)
-  )
-
-  const code =
-    await sock.requestPairingCode(number)
-
-  return code
-}
-
-app.get("/pair/:number", async (req, res) => {
-
-  try {
-
-    const code =
-      await startBot(req.params.number)
-
-    res.json({
-      success: true,
-      code
-    })
-
-  } catch (err) {
-
-    res.json({
-      success: false,
-      error: err.message
-    })
-
-  }
-
+res.send("AKATSUKII-MD ONLINE ✅")
 })
 
 app.listen(PORT, () => {
-  console.log(`Server running on ${PORT}`)
+console.log("Server running on ${PORT}")
 })
+
+async function startBot() {
+
+const { state, saveCreds } =
+await useMultiFileAuthState("./session")
+
+const { version } =
+await fetchLatestBaileysVersion()
+
+const sock = makeWASocket({
+version,
+auth: state,
+logger: Pino({ level: "silent" }),
+browser: ["AKATSUKII-MD", "Chrome", "1.0.0"]
+})
+
+sock.ev.on("creds.update", saveCreds)
+
+sock.ev.on("connection.update", async ({ connection }) => {
+
+if (connection === "open") {
+  console.log("✅ WhatsApp Connected")
+}
+
+})
+
+if (!state.creds.registered) {
+
+const code =
+  await sock.requestPairingCode("27687085163")
+
+console.log(`
+
+========================
+PAIR CODE
+${code}
+
+`)
+}
+
+sock.ev.on("messages.upsert", async ({ messages }) => {
+
+const msg = messages[0]
+if (!msg.message) return
+
+const from = msg.key.remoteJid
+
+const text =
+  msg.message.conversation ||
+  msg.message.extendedTextMessage?.text ||
+  ""
+
+if (text === ".ping") {
+  await sock.sendMessage(from, {
+    text: "🏓 PONG"
+  })
+}
+
+if (text === ".alive") {
+  await sock.sendMessage(from, {
+    text: "🤖 AKATSUKII-MD ONLINE"
+  })
+}
+
+})
+
+}
+
+startBot()
